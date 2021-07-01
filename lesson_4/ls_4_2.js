e3ed472cfeeb3fdbca5c6e3e81d51829c49ca850
k_yelaman <k.elaman2701@gmail.com>
2021-07-01 19:47:50 +0600

    fix ls_4_2.js

--- lesson_4/ls_4_2.js
+++ lesson_4/ls_4_2.js
@@ -1,27 +1,19 @@
 'use strict';
 
-const cart = { //наименование товара, количество
-  Яблоки: 2,
-  Молоко: 1,
-//   [Symbol.iterator]: function*() {
-//      yield this.Яблоки;
-//      yield this.Молоко;
-//    },
- };
-
-const goodsAndPrice = { //наименование товара, цена за 1 единицу(кг, литр, упаковка)
-  Яблоки: 100,
-  Шоколадка: 50,
-  Молоко: 55,
-  Мука: 30,
+const cart = {
+  goods: [
+    ['Яблоки', 100, 2], 
+    ['Шоколадка', 50, 3],
+    ['Молоко', 55, 1],
+    ['Мука', 30, 2],
+  ],
+  countBasketPrice() {
+    let price = 0;
+    for (let i = 0; i < this.goods.length; i++) {
+      price += this.goods[i][1] * this.goods[i][2];
+    }
+    return price;
+  }
 };
 
-function countBasketPrice(cart) {
-  let price = 0;
-  for (const prop in cart) {
-    price += (cart[prop] * goodsAndPrice[prop]);
-  };
-  return price;
-};
-
-console.log("Стоимость корзины: " + countBasketPrice(cart));
+console.log("Стоимость корзины: " + cart.countBasketPrice());
